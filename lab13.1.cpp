8b766b3b73544054f0a2321641eb4a2768b43655
enderdaniil <54796186+enderdaniil@users.noreply.github.com>
2019-10-28 10:35:29 +0300

    Update lab13.1.cpp

--- lab13.1.cpp
+++ lab13.1.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
 int main()
 {
-  cout<<"Vvedite N"<<endl;
-  int n;
-  cin>>n;
-  int arr[n];
-  for (int i=0; i<n; i++)
-  {
-    arr[i]=i*2+1;
-    cout<<arr[i]<<endl;
-  }
-  return 0;
+	cout << "Vvedite N" << endl;
+	int n;
+	cin >> n;
+
+	vector<int> arr(n);
+
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = i * 2 + 1;
+		cout << arr[i] << endl;
+	}
+	return 0;
 }
