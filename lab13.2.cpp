55ad8138da48a173786220e3c49659317d4d20e0
enderdaniil <54796186+enderdaniil@users.noreply.github.com>
2019-10-28 10:37:08 +0300

    Update lab13.2.cpp

--- lab13.2.cpp
+++ lab13.2.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
 #include <math.h>
+#include <vector>
+
 using namespace std;
 int main()
 {
-  cout<<"Vvedite N"<<endl;
-  int n;
-  float a,d;
-  cin>>n;
-  cout<<"Vvedite A and D"<<endl;
-  cin>>a>>d;
-  float arr[n];
-  for (int i=0; i<n; i++)
-  {
-    arr[i]=a*pow(d,i);
-    cout<<arr[i]<<endl;
-  }
-  return 0;
+	cout << "Vvedite N" << endl;
+	int n;
+	float a, d;
+	cin >> n;
+	cout << "Vvedite A and D" << endl;
+	cin >> a >> d;
+	vector<float> arr(n);
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = a * pow(d, i);
+		cout << arr[i] << endl;
+	}
+	return 0;
 }
